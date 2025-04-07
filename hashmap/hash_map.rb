d7c8b69fde86efb34a90dad8f552907a90a6f16a
petar101 <53666612+petar101@users.noreply.github.com>
2025-04-08 09:11:51 +1000

    created hash method

---
+++ hashmap/hash_map.rb
@@ -0,0 +1,16 @@
+class HashMap
+
+  attr_reader :capacity, :load_factor
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end
+ 
+
+
+end
