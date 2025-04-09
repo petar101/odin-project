a8e208511df2a2c51f7036dcd3fea2ad6821bceb
petar101 <53666612+petar101@users.noreply.github.com>
2025-04-10 09:44:34 +1000

    created buckets and inprogress to assigning correct key to bucket

--- hashmap/hash_map.rb
+++ hashmap/hash_map.rb
@@ -1,6 +1,13 @@
 class HashMap
 
-  attr_reader :capacity, :load_factor
+  attr_reader :capacity, :load_factor ## ive assigned two variables as readers do i give them the value also now ? 
+
+
+  def initialize(capacity = 16, load_factor = 0.75)
+    @capacity = capacity
+    @load_factor = load_factor
+    @buckets = Array.new(capacity)  # Creates an array with 'capacity' number of nil elements
+  end
 
   def hash(key)
     hash_code = 0
@@ -10,7 +17,28 @@ class HashMap
        
     hash_code
   end
+
+  def set(key, value) 
+    result = hash(key)
+
+    ## for result.each, put the set in buckets(#result)
+
+       # write method to say for every hash value, take that set in the corresponding number for the bucket
+    # end goal, set automaically makes a hash to it and sends it to the right bucket. 
+    
+  end
  
 
 
 end
+
+
+
+
+
+
+# in the bucket there will be 1 node (hask key + value), nil or a chained node. 
+# hashes will have their own hash code which will redirect nodes to what bucket.
+# each bucket should be measured which the capacity they can withhold and load factor(0.75) then expand 
+# 
+
