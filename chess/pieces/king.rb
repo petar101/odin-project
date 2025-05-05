a55e1c8a1b8e6a74fa9d8473ca14c11037508870
petar101 <53666612+petar101@users.noreply.github.com>
2025-05-06 09:09:04 +1000

    on par to complete king attributes

--- chess/pieces/king.rb
+++ chess/pieces/king.rb
@@ -1,1 +1,35 @@
 # frozen_string_literal: true
+require_relative 'piece'
+
+class King < Piece
+  def symbol
+    color == :white ? '♚' : '♔'
+  end
+
+  def direction
+    [
+      [-1, -1], [-1, 0], [-1, 1],
+      [ 0, -1],          [ 0, 1],
+      [ 1, -1], [ 1, 0], [ 1, 1]
+    ]
+  end
+
+  def move(board)
+    moves = []
+
+    direction.each do |dr, dc| 
+      new_row = direction + @row 
+      new_col = direction + @col
+
+      if on_board?(new_row, new_col)
+        # same colour isn't on new_row new_col 
+        # then move 
+        #
+      end
+    end
+
+  
+    
+    moves
+  end
+end
